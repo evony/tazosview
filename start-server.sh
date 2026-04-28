acb894fc3c29c1a84c9e4169ad425225bf636a24
Z User <z@container>
2026-04-28 19:28:04 +0000

    ad6962a0-b1b9-46f0-85dd-ce084bf97894

---
+++ start-server.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export DATABASE_URL="postgresql://neondb_owner:npg_epghiw6q0vVa@ep-red-lab-a174k45q-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+export DIRECT_DATABASE_URL="postgresql://neondb_owner:npg_epghiw6q0vVa@ep-red-lab-a174k45q-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+cd /home/z/my-project
+exec npx next dev -p 3000
